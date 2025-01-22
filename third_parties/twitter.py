33a9d627e36351485d88d0c1c7c26c11c04e8aa4
SanketShahane8 <sanket.shahane@outlook.com>
2025-01-22 20:08:22 +0530

    test commit

--- third_parties/twitter.py
+++ third_parties/twitter.py
@@ -17,7 +17,7 @@ twitter_client = tweepy.Client(
 
 def scrape_user_tweets(username, num_tweets=5):
     """
-    Scrapes a Twitter user's original tweets (i.e., not retweets or replies) and returns them as a list of dictionaries.
+    Scrapes a Twitter user's original tweets (i.e., not retweets and replies) and returns them as a list of dictionaries.
     Each dictionary has three fields: "time_posted" (relative to now), "text", and "url".
     """
     user_id = twitter_client.get_user(username=username).data.id
